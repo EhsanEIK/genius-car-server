2783fa4668050a736e1cca9f327e9fc41d32a61d
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-02 17:30:04 +0600

    added patch method to update status from client side to server side and then database

--- index.js
+++ index.js
@@ -49,6 +49,19 @@ async function run() {
             res.send(result);
         })
 
+        app.patch('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const status = req.body.status;
+            const query = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    status: status,
+                }
+            }
+            const result = await ordersCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
