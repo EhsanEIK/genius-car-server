946d613718cb356d752169f48987a493dd1f4de7
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-02 10:57:16 +0600

    connected to mongodb and added services collection and set api to get all services from db

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+
+// const uri = ${process.env.DB_LOCALHOST};
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fbieij7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const servicesCollection = client.db('geniusCarDB').collection('services');
+
+        // service api
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally { }
+}
+run().catch(error => console.error(error));
+
 
 app.get('/', (req, res) => {
     res.send('genius car server is running');
